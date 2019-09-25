7c3105ebe43647e2f0f782e34cb73811423e1625
Martin Berg <martin.yngve.berg@gmail.com>
2019-09-25 17:42:09 +0200

    skapat en array: flaskor

--- Sodaproject/Program.cs
+++ Sodaproject/Program.cs
@@ -8,30 +8,23 @@ namespace Sodaproject
 {
     class Sodacrate
     {
-        private int[] flaskor = new int[24];
-        private int antal_flaskor = 0; //Håller reda på antal flaskor
+        private Soda[] flaskor = new Soda[24];
+        public int antal_flaskor = 0; //Håller reda på antal flaskor
 
-        //listan med läsker definieras
-        List<Soda> listan;
+       
 
         public void Run()
         {
 
             //skapar 4 objekt av klassen soda. De ges namn, pris och dryckestyp
-            var soda1 = new Soda("Coca Cola", "läsk", 5);
-            var soda2 = new Soda("Coca Cola Zero", "lightläsk", 5);
-            var soda3 = new Soda("Hallonsoda", "läsk", 6);
-            var soda4 = new Soda("Ramlösa", "mineralvatten", 4);
+
+            Soda soda1 = new Soda("Coca Cola", "läsk", 5);
+            Soda soda2 = new Soda("Coca Cola Zero", "lightläsk", 5);
+            Soda soda3 = new Soda("Hallonsoda", "läsk", 6);
+            Soda soda4 = new Soda("Ramlösa", "mineralvatten", 4);
             //de här objekten kan man sedan välja på i add_soda 
             
-            //skapar en lista där objekten återfinns. 
-            listan = new List<Soda>();
-            {
-                listan.Add(soda1);
-                listan.Add(soda2);
-                listan.Add(soda3);
-                listan.Add(soda4);
-            }
+            
 
             do
             {
@@ -76,10 +69,10 @@ namespace Sodaproject
             Console.Clear();
             //Namnet från listan skrivs ut för vardera läsk
             Console.WriteLine("Fyll din läskback med 25 läskflaskor. Välj en läsk åt gången:");
-            Console.WriteLine("[1]" + listan[0].GetNamn());
-            Console.WriteLine("[2]" + listan[1].GetNamn());
-            Console.WriteLine("[3]" + listan[2].GetNamn());
-            Console.WriteLine("[4]" + listan[3].GetNamn());
+            Console.WriteLine("[1]" + flaskor);
+            Console.WriteLine("[2]" + flaskor);
+            Console.WriteLine("[3]" + flaskor);
+            Console.WriteLine("[4]" + flaskor);
             //+ ett val för att slumpa läsk
 
 
@@ -91,13 +84,21 @@ namespace Sodaproject
             //Vi går igenom läskbacken och lägger till en soda i varje position
             for (int i = 0; i < 25; i++)
             {
-                string str1 = Console.ReadLine(); //användarens val
-                int läskval = Convert.ToInt32(str1);//konverteras till en int då det ju är ett nummer vi får
+                //användaren skriver in ett nummer som sparas som int
+                string str1 = Console.ReadLine(); 
+                int läskval = Convert.ToInt32(str1);
 
-                //beroende på numret på läskval läggs en läsk på listan från ett givet index, in i läskbacken
+                //beroende på numret på läskval läggs ett objekt av soda in i läskbacken
+
+                //om användaren trycker 1, läggs soda1 in i läskbacken, plats 1:
                 
+
             }
+
         }
+
+
+        
 
         public void print_crate()
         {
@@ -183,4 +184,4 @@ namespace Sodaproject
             Console.ReadKey(true);
         }
     }
-}
+} 
