dec76d433a7e8c146592360a0e4111bdeba62c49
Sofia Nordgren <sofia.nordgren91@hotmail.com>
2019-10-04 11:14:47 +0200

    Kommenterat lite

--- Sodaproject/Program.cs
+++ Sodaproject/Program.cs
@@ -10,9 +10,10 @@ namespace Sodaproject
     class Sodacrate
     {
         //Här skapas en vektor som kan innehålla objekt av klassen soda, som i sin tur har olika egenskaper.
-        private Soda[] flaskor = new Soda[24];
+        private Soda[] sodaCrateArray = new Soda[25];
         int menyval; // deklareras här för att användas i metoden Run()
-        private int i;
+
+        private int i;//deklareras här för att användas i metoden add_soda
    
         public void Run()
         {
@@ -79,7 +80,7 @@ namespace Sodaproject
                 Console.WriteLine("Backen är full");
             }*/
 
-            //De olika läsksorterna skrivs ut
+            //De olika läsksorterna att välja på skrivs ut
             Console.WriteLine("Fyll din läskback med 25 läskflaskor. Välj en läsk åt gången:");
             Console.WriteLine("[1] Coca cola, 5 kr");
             Console.WriteLine("[2] Coca Cola Zero, 5 kr");
@@ -95,12 +96,8 @@ namespace Sodaproject
 
 
             //Här kan användaren via en for-loop och knapptryckningar lägga in en läsk i backen i taget. 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 25; i++)
                 {
-      
-                    
-                   
-
                     //användaren skriver in ett nummer som sparas som int
                     string str1 = Console.ReadLine();
                     int läskval = Convert.ToInt32(str1);
@@ -109,67 +106,65 @@ namespace Sodaproject
                     //om exempelvis läskval = 1 ska soda1 läggas i läskbacksvektorn
                     switch (läskval)
                     {
-
-
                         case 1:
                         Soda soda1 = new Soda("Coca Cola", "läsk", 5);
-                        flaskor[i] = soda1;
+                        sodaCrateArray[i] = soda1;
                         Console.WriteLine("Du lade till " + soda1.GetNamn());
                         break;
 
                         case 2:
                         Soda soda2 = new Soda("Coca Cola Zero", "lightläsk", 5);
-                        flaskor[i] = soda2;
+                        sodaCrateArray[i] = soda2;
                         Console.WriteLine("Du lade till " + soda2.GetNamn());
                         break;
 
 
                         case 3:
                         Soda soda3 = new Soda("Hallonsoda", "läsk", 6);
-                        flaskor[i] = soda3;
+                        sodaCrateArray[i] = soda3;
                         Console.WriteLine("Du lade till " + soda3.GetNamn());
                         break;
 
 
                         case 4:
                         Soda soda4 = new Soda("Sockerdricka", "läsk", 4);
-                        flaskor[i] = soda4;
+                        sodaCrateArray[i] = soda4;
                         Console.WriteLine("Du lade till " + soda4.GetNamn());
                         break;
 
                         case 5:
                         Soda soda5 = new Soda("Fanta", "läsk", 5);
-                        flaskor[i] = soda5;
+                        sodaCrateArray[i] = soda5;
                         Console.WriteLine("Du lade till " + soda5.GetNamn());
                         break;
 
                         case 6:
                         Soda soda6 = new Soda("Grappo", "läsk", 5);
-                        flaskor[i] = soda6;
+                        sodaCrateArray[i] = soda6;
                         Console.WriteLine("Du lade till " + soda6.GetNamn());
                         break;
 
                         case 7:
                         Soda soda7 = new Soda("Ramlösa", "mineralvatten", 4);
-                        flaskor[i] = soda7;
+                        sodaCrateArray[i] = soda7;
                         Console.WriteLine("Du lade till " + soda7.GetNamn());
                         break;
 
                         case 8:
                         Soda soda8 = new Soda("Loka Citron", "mineralvatten", 4);
-                        flaskor[i] = soda8;
+                        sodaCrateArray[i] = soda8;
                         Console.WriteLine("Du lade till " + soda8.GetNamn());
                         break;
 
                         case 9:
                         Soda soda9 = new Soda("Norrlands Guld 3,5%", "öl", 6);
-                        flaskor[i] = soda9;
+                        sodaCrateArray[i] = soda9;
                         Console.WriteLine("Du lade till " + soda9.GetNamn());
                         break;
 
                         case 10:
                         Soda soda10 = new Soda("Pripps blå 3,5%", "öl", 6);
-                        flaskor[i] = soda10;
+                        sodaCrateArray[i] = soda10;
                         Console.WriteLine("Du lade till " + soda10.GetNamn());
                         break;
 
@@ -178,38 +173,36 @@ namespace Sodaproject
 
                 }
 
-            
-
         }
 
         public void remove_soda()
         {
-            
+            //metoden ersätter en läsk i backen med en annan
             Console.Clear();
 
             Console.WriteLine("Backen innehåller:");
-            for (int i = 0; i < 5; i++)
-            {
+            for (int i = 0; i < 25; i++)
+            {//går först igenom vektorn i Sodacrate och kontrollerar om platsen är tom, vilket då skrivs ut
+             //om det finns en läsk på varje plats, skrivs namn på läsken ut.
 
-                if (flaskor[i] == null)
+                if (sodaCrateArray[i] == null)
                 {
                     Console.WriteLine("Platsen är tom!");
                 }
 
 
-                else if (flaskor[i] != null)
+                else if (sodaCrateArray[i] != null)
                 {
-                    Console.WriteLine(i+1 + ". " + flaskor[i].GetNamn());
+                    //Här skrivs namnen ut och numret på positionen skrivs ut framför det
+                    Console.WriteLine(i+1 + ". " + sodaCrateArray[i].GetNamn());
                 }
 
             }
-            if (flaskor[i] != null)
+            if (sodaCrateArray[i] != null)
             {
-
-
-                //print_crate();
+                //om det finns en läsk på varje plats i vektorn får användaren välja en plats i den
                 Console.WriteLine("För att ändra läsk, välj dess plats i backen:");
-                string str = Console.ReadLine(); //användarens val
+                string str = Console.ReadLine(); 
                 int flaska = Convert.ToInt32(str);
 
 
@@ -235,61 +228,61 @@ namespace Sodaproject
                     case 1:
 
                         Soda soda1 = new Soda("Coca Cola", "läsk", 5);
-                        flaskor[flaska - 1] = soda1;
+                        sodaCrateArray[flaska - 1] = soda1;
                         break;
 
                     case 2:
 
                         Soda soda2 = new Soda("Coca Cola Zero", "lightläsk", 5);
-                        flaskor[flaska - 1] = soda2;
+                        sodaCrateArray[flaska - 1] = soda2;
                         break;
 
 
                     case 3:
                         Soda soda3 = new Soda("Hallonsoda", "läsk", 6);
-                        flaskor[flaska - 1] = soda3;
+                        sodaCrateArray[flaska - 1] = soda3;
                         break;
 
 
                     case 4:
                         Soda soda4 = new Soda("Sockerdricka", "läsk", 4);
-                        flaskor[flaska - 1] = soda4;
+                        sodaCrateArray[flaska - 1] = soda4;
                         break;
 
                     case 5:
                         Soda soda5 = new Soda("Fanta", "läsk", 5);
-                        flaskor[flaska - 1] = soda5;
+                        sodaCrateArray[flaska - 1] = soda5;
                         break;
 
                     case 6:
                         Soda soda6 = new Soda("Grappo", "läsk", 5);
-                        flaskor[flaska - 1] = soda6;
+                        sodaCrateArray[flaska - 1] = soda6;
                         break;
 
                     case 7:
                         Soda soda7 = new Soda("Ramlösa", "mineralvatten", 4);
-                        flaskor[flaska - 1] = soda7;
+                        sodaCrateArray[flaska - 1] = soda7;
                         break;
 
                     case 8:
                         Soda soda8 = new Soda("Loka citron", "mineralvatten", 4);
-                        flaskor[flaska - 1] = soda8;
+                        sodaCrateArray[flaska - 1] = soda8;
                         break;
 
                     case 9:
                         Soda soda9 = new Soda("Norrlands Guld", "öl", 6);
-                        flaskor[flaska - 1] = soda9;
+                        sodaCrateArray[flaska - 1] = soda9;
                         break;
 
                     case 10:
                         Soda soda10 = new Soda("Pripps blå", "öl", 6);
-                        flaskor[flaska - 1] = soda10;
+                        sodaCrateArray[flaska - 1] = soda10;
                         break;
 
                 }
             }
 
-            else if (flaskor[i] == null)
+            else if (sodaCrateArray[i] == null)
             {
                 Console.WriteLine("Läskbacken är tom. Tryck valfri tangent för att återgå till menyn");
                 Console.ReadKey();
@@ -306,17 +299,17 @@ namespace Sodaproject
            
 
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 25; i++)
             {
-                if (flaskor [i]!=null)
+                if (sodaCrateArray [i]!=null)
                 {
 
-                Console.WriteLine(flaskor[i].GetNamn() + ", " + flaskor[i].GetDryckestyp());
+                Console.WriteLine(sodaCrateArray[i].GetNamn() + ", " + sodaCrateArray[i].GetDryckestyp());
                 }
               
             }
 
-           if (flaskor[i]== null)
+           if (sodaCrateArray[i]== null)
             {
                 Console.WriteLine("Backen är tom. Tryck valfri tangent för att återgå till menyn");
                 Console.ReadKey();
@@ -335,13 +328,13 @@ namespace Sodaproject
 
             int sum = 0;
             
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 25; i++)
             {
               
-                if(flaskor [i]!= null)
+                if(sodaCrateArray [i]!= null)
                 {
                     flaskorFinns = true;
-                    sum = sum + flaskor[i].GetPris();
+                    sum = sum + sodaCrateArray[i].GetPris();
                 }
 
 
